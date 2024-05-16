b0db5d49aa1844d57ba432e9cc5d14c68eb6eb9b
Gabriel Gomez <gomezg@tamu.edu>
2024-05-16 12:14:43 -0500

    Modiffied RCC and added detailed pseudocode for SwapEdges

--- experimental/algorithm/LAGraph_RichClubCoefficient.c
+++ experimental/algorithm/LAGraph_RichClubCoefficient.c
@@ -43,9 +43,12 @@
     GrB_free (&degrees) ;                   \
     GrB_free (&edge_gt_deg) ;               \
     GrB_free (&edge_eq_deg) ;               \
+    GrB_free (&edge_adjusted_deg) ;         \
     GrB_free (&cumulative_deg) ;            \
     GrB_free (&edges_per_deg) ;             \
+    GrB_free (&cumulative_edges) ;          \
     GrB_free (&two_one) ;                   \
+    GrB_free (&rcCalculation) ;             \
 }
 
 #define LG_FREE_ALL                         \
@@ -61,16 +64,13 @@
 #include "LAGraphX.h"
 
 //taken from LAGraph_BF_full_mxv
-//Is this best way to do this?
 typedef void (*LAGraph_binary_function) (void *, const void *, const void *) ;
 
-//TODO: Add macro for string
 #define TWO_ONE_ADD                                                         \
     "void two_one_add(uint64_t *z, const uint64_t *x, const uint64_t *y)"   \
     "{"                                                                     \
         "(*z) = 2 * (*x) + (*y) ;"                                          \
     "}"
-//I am hoping this gets done in a single leaq.
 void two_one_add(uint64_t *z, const uint64_t *x, const uint64_t *y)
 { 
     (*z) = 2 * (*x) + (*y);
@@ -98,7 +98,7 @@ int LAGraph_RichClubCoefficient
     char *msg
 )
 {
-
+    return (GrB_NOT_IMPLEMENTED) ;
     //--------------------------------------------------------------------------
     // Declorations
     //--------------------------------------------------------------------------
@@ -126,18 +126,19 @@ int LAGraph_RichClubCoefficient
     // account for double counting in edge_eq_deg
     GrB_Vector edge_adjusted_deg = NULL;
 
+    //the ith entry contains the number of nodes with degree greater than i.
+    GrB_Vector cumulative_deg = NULL;
+
     // the ith entry contains the number of edges whos lowest degree is i.
     GrB_Vector edges_per_deg = NULL;
 
-    //the ith entry contains the number of nodes with degree greter than i.
-    GrB_Vector cumulative_deg = NULL;
-
-    //the ith entry contains the number of edges among 
+    //the ith entry contains twice the number of edges among 
     // nodes with degree greater than i.
     GrB_Vector cumulative_edges = NULL;
 
     // 2x+y
     GrB_BinaryOp two_one = NULL;
+
     // 2E_K / (N_k (N_k -1))
     GrB_BinaryOp rcCalculation = NULL;
 
@@ -153,11 +154,9 @@ int LAGraph_RichClubCoefficient
     //Should the arrays be initialized before fed to the function?
     GrB_Index *index_edge = NULL; // does this need to be null to start with? 
     
-    //TODO: check if this is void or GrB_UINT64
     uint64_t *edge_count_per_node = NULL, *deg_arr = NULL, 
-        *edges_per_deg_arr = NULL, *cumul_array = NULL;
-
-
+        *edges_per_deg_arr = NULL, *cumul_array = NULL, *ones = NULL, 
+        *ramp = NULL;
 
     //TODO: YOU STILL HAVE TO GRBFREE THIS WITH A LOOP? 
     GrB_Index **index_degree = NULL; // does this need to be null to start with?
@@ -185,6 +184,7 @@ int LAGraph_RichClubCoefficient
     //--------------------------------------------------------------------------
     A = G->A ;
     GRB_TRY(GrB_Matrix_nrows (&n, A)) ;
+    GRB_TRY(GrB_Matrix_new(&D, GrB_UINT64, n, n))
     GRB_TRY(GrB_Matrix_new(&edge_degrees, GrB_UINT64,n,n)) ;
     GRB_TRY(GrB_Vector_new(&degrees, GrB_UINT64, n)) ;
     GRB_TRY(GrB_Vector_new(&edge_gt_deg, GrB_UINT64, n)) ;
@@ -200,33 +200,56 @@ int LAGraph_RichClubCoefficient
         GrB_UINT64, GrB_UINT64, GrB_UINT64, 
         "rich_club_formula", RICH_CLUB_FORMULA)) ;
             
-    // code from LAGraph_MaximalIndependentSet
-    // degrees = G->out_degree (check if this is needed) (I think no)
+    // degrees = G->out_degree
     GRB_TRY (GrB_assign(
         degrees, NULL, NULL, G->out_degree, GrB_ALL, n, NULL)) ;
 
     GRB_TRY (GrB_Matrix_diag(&D, degrees, 0)) ;
 
-    //Assigns each edge in the graph the value of their column node
-    //TODO: IDK what the descriptor is
+    // Each edge in the graph gets the value of the degree of its column node
     GRB_TRY (GrB_mxm(
-        edge_degrees, NULL, NULL, GxB_ANY_SECOND_UINT64,A,D,GrB_DESC_T1)) ;
+        edge_degrees, NULL, NULL, GxB_ANY_SECOND_UINT64,A,D, GrB_NULL)) ;
     
-    //Adds up all the edges whose rows degrees are greater than their column degrees
-    GRB_TRY(GrB_vxm(
-        edge_gt_deg, NULL, NULL, GxB_PLUS_ISGT_UINT64, 
-        degrees, edge_degrees, GrB_DESC_T1)) ;
+    // QUESTION: Mentioned GxB would be slower than JIT correct?
 
-    //Adds up all the edges whose rows degrees are greater than their column degrees
-    //Double check that this can return zeros and not just novals
+    // Counts the edges for which its row node is the node of smallest degree.
+    // Simply, this edge would be removed from the graph once this node
+    // is of degree < k. 
+    GRB_TRY(GrB_vxm(
+        edge_gt_deg, NULL, NULL, GxB_PLUS_ISLT_UINT64, 
+        degrees, edge_degrees, GrB_NULL)) ;
+
+    // Counts the edges for which its row node and col nodes have equal degree.
+    // Simply, this edge would be removed from the graph once this node
+    // is of degree < k. 
     GRB_TRY(GrB_vxm(
         edge_eq_deg, NULL, NULL, GxB_PLUS_ISEQ_UINT64, 
-        degrees, edge_degrees, GrB_DESC_T1)) ;
+        degrees, edge_degrees, GrB_NULL)) ;
 
-    // edge_eq_deg and edge_gt_deg have same non zero pattern
+
+    // QUESTION: Is JIT faster or should I use accum as seen below?
+
+    // If the nodes of an edge have different degrees, the edge is counted once.
+    // If they have the same degree, that edge is double counted. So, we adjust:
+    // edge_adjusted_deg = 2 * edge_gt_deg + edge_eq_deg 
+
     GRB_TRY(GrB_eWiseMult(
         edge_adjusted_deg, NULL, NULL, two_one, 
-        edge_eq_deg, edge_gt_deg, GrB_DESC_T1));
+        edge_gt_deg, edge_eq_deg, GrB_NULL));
+
+    /* 
+    // Actually why not do this, and skip an operation?
+
+    GRB_TRY(GrB_vxm(
+        edge_gt_deg, NULL, NULL, GxB_PLUS_ISLT_UINT64, 
+        degrees, edge_degrees, GrB_NULL)) ;
+    GRB_TRY(GrB_vxm(
+        edge_gt_deg, NULL, two_one_add, GxB_PLUS_ISEQ_UINT64, 
+        degrees, edge_degrees, GrB_NULL)) ;
+
+    // --- Okay well at that point why not just do it all in one multiply? ---
+    // with this semiring [+].[(x == y) + 2 * (x < y)]
+     */
 
     GRB_TRY(GrB_Vector_nvals (&edge_vec_nvals, edge_adjusted_deg));
     vi_size = (edge_vec_nvals+1)*sizeof(GrB_Index);
@@ -234,45 +257,57 @@ int LAGraph_RichClubCoefficient
 
     GRB_TRY(GxB_Vector_unpack_CSC(
         edge_adjusted_deg, &index_edge, (void **) &edge_count_per_node,
-        &vi_size,&vx_size,&iso,&edge_vec_nvals,NULL, GrB_DESC_T1));
+        &vi_size,&vx_size,&iso,&edge_vec_nvals,NULL, GrB_NULL));
     LG_TRY(LAGraph_Free((void **)&index_edge, NULL)) ;
 
     GRB_TRY(GxB_Vector_unpack_CSC(
         degrees, &index_edge, (void **) &deg_arr,
-        &vi_size,&vx_size,&iso,&edge_vec_nvals,NULL, GrB_DESC_T1));
+        &vi_size,&vx_size,&iso,&edge_vec_nvals,NULL, GrB_NULL));
 
     LG_TRY(LAGraph_Free((void **)&index_edge, NULL)) ;
+
     //Build with degrees as indecies and handle duplicates via adition
     GRB_TRY(GrB_Vector_build (
         edges_per_deg, deg_arr, edge_count_per_node, n, GrB_PLUS_INT64)) ;
+
+    // Start ones. Do I do this by building an iso vector? or by myself?
+    // QUESTION: I want to do the following. I should duplicate deg_arr memory correct?
+    GRB_TRY(GrB_Vector_build (
+        cumulative_deg, deg_arr, ones, n, GrB_PLUS_INT64)) ;
     GRB_TRY(GxB_Vector_unpack_CSC(
-        edges_per_deg, &index_edge, edges_per_deg_arr,
-        &vi_size,&vx_size,&iso,&edge_vec_nvals,NULL, GrB_DESC_T1));
+        edges_per_deg, &index_edge, &edges_per_deg_arr,
+        &vi_size, &vx_size, &iso, &edge_vec_nvals, NULL, GrB_NULL));
     
     //run a cummulative sum (backwards) on edges_per_deg_arr
     // for loops
-    
     for(uint64_t i = edge_vec_nvals; i > 1; i--)
     {
         edges_per_deg_arr[i-1]+=edges_per_deg_arr[i];
     }
-    
-
     //Construct a vector thats has edges_per_deg_arr values but repeated whenever 
     //index_edge has a skip and put into cumulative edges
     // ie. [0,1,6,7,10] & [9,7,3,2,0] ->
-    // [9,7,7,7,7,7,3,2,2,2,0, . . .]
+    // [9,7,7,7,7,7,3,2,2,2, . . .]
 
-    uint64_t index = 0 ;
+    uint64_t index = 0, i = 0;
     LAGraph_Malloc((void **) &cumul_array, n, sizeof(uint64_t), msg) ;
 
-    for(uint64_t i = 0; i < n; i++)
+    for(; i < n; i++) // seems easily parrallelizable but idk if #pragma is enough.
     {
-        edges_per_deg_arr[index] ;
-
-        if(index < edge_vec_nvals && i == index_edge[index])
+        cumul_array[i] = edges_per_deg_arr[index];
+        if(i == index_edge[index])
+        {
             index++ ;
+            if(index == edge_vec_nvals || edges_per_deg_arr[index] == 0)
+                break ;
+        }
     }
+    // QUESTION: should this be i - 1? Do I build ramp in the forloop above?
+    GRB_TRY(GrB_Vector_build(
+        cumulative_edges, ramp, cumul_array, i, NULL
+    )) ;
+
+
 
     //GrB select or just do an if test
     LG_FREE_WORK ;
--- experimental/algorithm/LAGraph_SwapEdges.c
+++ experimental/algorithm/LAGraph_SwapEdges.c
@@ -19,7 +19,6 @@
 #define LG_FREE_WORK                        \
 {                                           \
     /* free any workspace used here */      \
-    GrB_free (&W) ;                         \
 }
 
 #define LG_FREE_ALL                         \
@@ -27,7 +26,6 @@
     /* free any workspace used here */      \
     LG_FREE_WORK ;                          \
     /* free all the output variable(s) */   \
-    GrB_free (&Y) ;                         \
     /* take any other corrective action */  \
 }
 
@@ -43,6 +41,7 @@ int LAGraph_HelloWorld // a simple algorithm, just for illustration
     char *msg
 )
 {
+    return (GrB_NOT_IMPLEMENTED) ;
     //--------------------------------------------------------------------------
     // Declorations
     //--------------------------------------------------------------------------
@@ -54,18 +53,71 @@ int LAGraph_HelloWorld // a simple algorithm, just for illustration
     // Extract adjacency matrix to make incidence matrix - E
 
     // While less than nvals*swapsperedge swaps have happened
-        // Pair edges into M1 and M2 matricies (HOW?)
-        // M <-- M1 + M2
-        // Get MT
+
+        // Coming into the loop I want E to be the incidence matrix of the new 
+        // graph. W/o self edges nor parallel edges. Each row must have
+        // exactly one 0 and one 1. They do not have to be randomly assigned.
+
+        // Pair edges. Take a random permutation and pair adjacent values.
+        // pairs wants:
+        // rows: [0, 0, 1, 1, 2, 2, 3, 3, . . .] (ramp / 2)
+        // cols: [ random permutation 1 - nvals] (LAGraph_Random_Seed + sort)
+        // vals: [0, 3, 0, 2, 0, 2, 0, 3, . . .]
+            // Start with [0, 2, 0, 2, 0, 2, . . .]
+            // for i from 1 to nvals by 2s:
+            //      vals[i] += vals[i] > vals[i-1]
+        // CAUTION cols is likely smaller. Manage memory correctly.
+
+        // each row of M will have 4 values [0,1,2,3]
+        // pairs |  E  |  M
+        //   0   | 0,1 | 0,1
+        //   2   | 0,1 | 2,3
+        //   3   | 0,1 | 3,2
+        // This gives us randomization as to which type of swap is happening.
+        // M = pairs * E (any_bitwisexor)
+
         // remove and entries of M with less than 3 values
-        // Swap interference <-- MT plus_one M
-        // Extract >=2 masking the diagonal
-        // stop any swaps with a row in Swap interference w/ degree > 1
-        // do remaining swaps. (HOW?)
-        // update E
+
+        // probably worth it to put 0s on the interf diagonal to make the 
+        // following vector dense.
+        // interf <diagonal> = MT * M (plus_one)
+        // reduce interf to a vector on rows
+        // r_interf = interf * denseVec (max_first)
+        // d_interf = diagonalize r_interf
+
+        // set 3rd bit to 1 if it must be removed. 
+        // An accumulator might work better.
+        // M = d_interf * M (any_[z = 4 * (x > 2) | y]) 
+        // M = extract M (x & 4 == 0)
+
+        // M1 = extract M (x & 2 == 0)
+        // M2 = extract M (x & 2 != 0)
+        // Check if an edge already exists in the graph.
+        // If a row of M1 or M2 has more than 1 vertex in common with a 
+        // row of E, the value of that entry in the exists array will be 0. 
+        // Otherwise, 1 or noval.
+        // exists = E * M1T (zero_one)
+        // exists += E * M2T (zero_one) (accum is one if theres an intersection)
+
+        // 0 if intended swap already exists in the matrix, 1 or noval otherwise
+        // r_exists = denseVec * exists (bitwiseAnd_second)
+        // QUESTION: how to remove r_exists zeros from M matrix?
+
+        // Compute S. note we could do ST if its better.
+        // pairs |  M  |  S
+        //   0   | 0,1 | 0,1
+        // 2 / 3 | 0,1 | 2,3
+        //   0   | 2,3 | 2,3
+        // 2 / 3 | 2,3 | 0,1
+        // S = pairsT * M (any_[z = (x & 2) ^ y])
+        // S = extract S (x & 2 == 0)
+
+        // If S has a row, Remove it from E and replace with the row from S
+        // QUESTION
+
+        // update E and possibly ET
         
 
     LG_FREE_WORK ;
-    (*Yhandle) = Y ;
     return (GrB_SUCCESS) ;
 }
